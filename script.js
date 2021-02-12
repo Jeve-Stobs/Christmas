0ea9d338159b9131ce16f17819bead925a7e507f
Samuel Casey <samuelacasey@gmail.com>
2021-02-12 16:13:49 +0000

    Beautify js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-var acountDownDate=new Date("Dec 25, 2021 00:00:00".replace(/-/g,"/")).getTime(),amyfunc=setInterval(function(){{var a=(new Date).getTime(),b=acountDownDate-a,c=Math.floor(b/864e5),d=Math.floor(b%864e5/36e5),e=Math.floor(b%36e5/6e4),f=Math.floor(b%6e4/1e3);Math.floor(b/10)}document.getElementById("adays").innerHTML=c+"d ",document.getElementById("ahours").innerHTML=d+"h ",document.getElementById("amins").innerHTML=e+"m ",document.getElementById("asecs").innerHTML=f+"s ",0>b&&(clearInterval(myfunc),document.getElementById("adays").innerHTML="",document.getElementById("ahours").innerHTML="",document.getElementById("amins").innerHTML="",document.getElementById("asecs").innerHTML="",document.getElementById("aend").innerHTML="It's christmas! Go get your presents! :D")},10);
+var acountDownDate = new Date("Dec 25, 2021 00:00:00".replace(/-/g, "/")).getTime(),
+  amyfunc = setInterval(function() {
+    {
+      var a = (new Date).getTime(),
+        b = acountDownDate - a,
+        c = Math.floor(b / 864e5),
+        d = Math.floor(b % 864e5 / 36e5),
+        e = Math.floor(b % 36e5 / 6e4),
+        f = Math.floor(b % 6e4 / 1e3);
+      Math.floor(b / 10)
+    }
+    document.getElementById("adays").innerHTML = c + "d ", document.getElementById("ahours").innerHTML = d + "h ", document.getElementById("amins").innerHTML = e + "m ", document.getElementById("asecs").innerHTML = f + "s ", 0 > b && (clearInterval(myfunc), document.getElementById("adays").innerHTML = "", document.getElementById("ahours").innerHTML = "", document.getElementById("amins").innerHTML = "", document.getElementById("asecs").innerHTML = "", document.getElementById("aend").innerHTML = "It's christmas! Go get your presents! :D")
+  }, 10);
